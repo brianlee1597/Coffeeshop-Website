9ab5dbb458a76e8d48b8bfa6e9f47c201c545d49
Brian Lee <canadaboy123@Brians-MacBook-Pro-2.local>
2021-01-07 16:12:38 +0900

    update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//to switch between content on the same section on whole beans nav.
+/*to switch between content on the same section on whole beans nav.
 function ShowContent(content) {
   document.getElementById("houseblend").style.display = "none";
   document.getElementById("singleorigin").style.display = "none";
@@ -14,4 +14,26 @@ document.addEventListener("DOMContentLoaded", function () {
 //jQuery for navigation button animation for whole beans section
 $(document).on("click", "nav ul li", function () {
   $(this).addClass("active").siblings().removeClass("active");
+});*/
+
+//Nav Begin
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener("click", () => {
+  //Animate Links
+  navLinks.classList.toggle("open");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
+
+  //Hamburger Animation
+  hamburger.classList.toggle("toggle");
+});
+
+//top Navbar script
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("nav");
+  header.classList.toggle("sticky", window.scrollY > 0);
 });
